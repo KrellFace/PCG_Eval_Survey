a8f6cc6c9f2e0be4c2a46e3a7fc5979687b5b48b
KrellFace <owithington@hotmail.co.uk>
2024-06-11 16:19:17 +0100

    Rough Notes Column Removed

--- main_PCGEval.py
+++ main_PCGEval.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 
-inputFile = 'Data/PCG Survey - Annotated V1.csv'
+inputFile = 'Data/PCG Survey - Annotated.csv'
 
 
 def generate_histogram(columns, counts, title, filename, horizontal = True, colour = "lightblue"):
